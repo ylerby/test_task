0fc3ed9cd6bbac2357cda84601aa5a8937032a70
ylerby <WeaklyFob@yandex.ru>
2023-09-11 22:09:08 +0300

    Сделано получение csv файла по HTTP-запросу, его декодированиее в UTF-8

--- test_task_app/views.py
+++ test_task_app/views.py
@@ -1,11 +1,26 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.views import View
-#import csv
+from test_task_app.forms import UrlForm
+import pandas
+import csv
+import requests
 
 
 class CsvView(View):
     @staticmethod
     def get(request):
-        url = request.GET.get("url", None)
-        return HttpResponse(f"<h1>Привет {url}<h1>")
+        url_form = UrlForm()
+        return render(request, "url_entering.html", context={"form": url_form})
 
+    @staticmethod
+    def post(request):
+        url = request.POST.get("url", None)
+        with requests.Session() as s:
+            download = s.get(url)
+
+            decoded_content = download.content.decode('utf-8')
+
+            cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+            my_list = list(cr)
+            return HttpResponse(f"{my_list[0]}")
