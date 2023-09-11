1acf5f23b7003d8ed36209d7cd316dde0d40d6d2
ylerby <WeaklyFob@yandex.ru>
2023-09-11 23:23:18 +0300

    Создана модель пользователя

--- test_task_app/models.py
+++ test_task_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    login = models.CharField()
+    password = models.CharField()
+    email = models.EmailField()
