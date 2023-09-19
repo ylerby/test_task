c94072311fc3f6bf2db41ef599974d75c2c1f2f0
ylerby <WeaklyFob@yandex.ru>
2023-09-19 23:42:04 +0300

    Доделана корректная сортировка и шаблон для нее

--- test_task_app/views.py
+++ test_task_app/views.py
@@ -30,16 +30,16 @@ class CsvView(View):
             cr = csv.reader(decoded_content.splitlines(), delimiter=',')
             data_list = list(cr)
 
-            # fixme: CSVFiles.objects.all().delete()
-
             if CSVFiles.objects.filter(Q(file_name=name) | Q(file_url=url)).exists():
                 return render(request, "already_exist.html")
 
+            # TODO: ИЗМЕНИТЬ ИНКРЕМЕНТАЦИЮ ID
+            # fixme: файл скачивается, но выводится сообщение "файл уже существует"
+            # fixme: ПРОБЛЕМА С ОГРАНИЧЕНИЕМ НА СИМВОЛЫ
+
             csv_file = CSVFiles.objects.create(file_name=name, file_url=url, file_column_name=" , ".join(data_list[0]))
             for i, column in enumerate(data_list[1:]):
-                CSVData.objects.create(column_id=csv_file, column_data=" , ".join(column), id=i + 1)
-
-            # split_column_name = column_names.split(",")
+                CSVData.objects.create(column_id=csv_file, column_data=" , ".join(column))
 
             return render(request, "successful_download.html")
 
@@ -53,7 +53,6 @@ class GetCsvView(View):
         return render(request, "get_csv_file.html", {"form": get_csv_form})
 
     # todo: заменить column_id на csv_file id
-    # todo: сделать сортировку полей !!!
     @staticmethod
     def post(request):
         csv_file_name: str = request.POST.get("file_name", None)
@@ -147,10 +146,11 @@ class FileSorting(View):
     def get(request) -> HttpResponse:
         file_sorting_form = FileSortingForm()
 
-        # todo: сделать получение url-параметра
         current_file_name = request.GET.get("file_name")
         current_file = CSVFiles.objects.get(file_name=current_file_name)
+
         number_of_columns = len(current_file.file_column_name.split(sep=',')) - 1
+
         return render(request, "file_sorting.html", {"form": file_sorting_form,
                                                      "number_of_columns": number_of_columns})
 
@@ -158,12 +158,16 @@ class FileSorting(View):
     def post(request):
         column_id = int(request.POST.get("column_id", None))
         current_file_name = request.GET.get("file_name")
+
         current_file = CSVFiles.objects.get(file_name=current_file_name)
         current_file_data = CSVData.objects.filter(column_id=current_file.id)
 
+        current_file_column = current_file.file_column_name.split(sep=",")[column_id]
         data_list = [column.column_data for column in current_file_data]
+
         sorted_data_list = list(sorted(data_list, key=lambda x: x.split(",")[column_id]))
-        return HttpResponse(f"<h1>{sorted_data_list}<h1>")
+        return render(request, "sorted_csv_file.html", {"data": sorted_data_list,
+                                                        "column_name": current_file_column})
 
 
 def main_page(request):
